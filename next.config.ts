de594113425e6546dcabd04aabd18feadc3467fa
Kassio Dias <sapokk.pinga@gmail.com>
2025-06-03 23:52:15 +0000

    studio-2208881342:~/studio{master}$ npm run dev
    > nextn@0.1.0 dev
    > next dev --turbopack -p 9003
    
       ▲ Next.js 15.2.3 (Turbopack)
       - Local:        http://localhost:9003
       - Network:      http://10.88.0.3:9003
       - Environments: .env
    
     ✓ Starting...
     ✓ Ready in 2.2s
     ⚠ Blocked cross-origin request from 9003-firebase-studio-1748989356421.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev. To allow this, configure "allowedDevOrigins" in next.config
    Read more: https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  allowedDevOrigins: ['https://9003-firebase-studio-1748989356421.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev'],
+  allowedDevOrigins: [
+    'https://9003-firebase-studio-1748989356421.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev',
+    'http://9003-firebase-studio-1748989356421.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev'
+  ],
 };
 
 export default nextConfig;
